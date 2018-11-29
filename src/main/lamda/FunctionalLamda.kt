57eba12ef8fad7679327461a2a973cc8ec70e2b1
Pavel Evleev <pavel.evleev@zensoft.io>
2018-11-29 23:02:57 +0300

    nullable type and lamda example

---
+++ src/main/lamda/FunctionalLamda.kt
@@ -0,0 +1,23 @@
+package main.lamda
+
+import kotlin.concurrent.thread
+
+fun postoneComputation(delay: Long, computation: () -> Unit) {
+    thread {
+        Thread.sleep(delay)
+        computation()
+    }
+}
+
+
+fun main(args: Array<String>) {
+
+    var i = 0
+    postoneComputation(5000) { println(42) }
+    while (i < 100) {
+        Thread.sleep(1000)
+        println(". . . ")
+        i++
+    }
+
+}
---
+++ src/main/lamda/Person.kt
@@ -0,0 +1,14 @@
+package main.lamda
+
+data class Person(val name: String, val age: Int)
+
+
+fun main(args: Array<String>) {
+    val people = listOf(Person("Pavel", 26), Person("Igor", 48))
+    println(people.maxBy { it.age })
+
+    val nextAction = ::sendEmail
+
+    people.forEach { nextAction(it, "привет жесть") }
+
+}
