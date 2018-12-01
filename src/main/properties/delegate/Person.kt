7aeb66115b5ecd3160f1599c1a3c5b8ae9298413
Pavel Evleev <mr.malefiic@gmail.com>
2018-12-01 17:28:04 +0300

    event listener on changing property is added  *PropertyChangeSupport

---
+++ src/main/properties/delegate/Person.kt
@@ -0,0 +1,17 @@
+package main.properties.delegate
+
+class Person(val name: String, age: Int, salary: Int) : PropertyChangeAware() {
+    var age: Int = age
+        set(newValue) {
+            val oldValue = field
+            field = newValue
+            changeSupport.firePropertyChange("age", oldValue, newValue)
+        }
+
+    var salary: Int = salary
+        set(newValue) {
+            val oldValue = field
+            field = newValue
+            changeSupport.firePropertyChange("salary", oldValue, newValue)
+        }
+}
